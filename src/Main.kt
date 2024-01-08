ca1749f41f190130fb88fa5e18dd1f5e5955aa26
Jose <jdiabus511@g.educaand.es>
2024-01-08 19:30:42 +0100

    correccion error

--- src/Main.kt
+++ src/Main.kt
@@ -2,19 +2,19 @@ fun main() {
     //
     var r2d2Pos: List<Int>
     r2d2Pos= arturito(10,5,-2)
-    println("x: ${r2d2Pos[0]}, y: ${r2d2Pos[1]}, direccion: ${r2d2Pos[2]}")
+    println("x: ${r2d2Pos[0]}, y: ${r2d2Pos[1]}, direccion: ${mostrarDireccion(r2d2Pos[2])}")
 
     r2d2Pos= arturito(0,0,0)
-    println("x: ${r2d2Pos[0]}, y: ${r2d2Pos[1]}, direccion: ${r2d2Pos[2]}")
+    println("x: ${r2d2Pos[0]}, y: ${r2d2Pos[1]}, direccion: ${mostrarDireccion(r2d2Pos[2])}")
 
     r2d2Pos= arturito()
-    println("x: ${r2d2Pos[0]}, y: ${r2d2Pos[1]}, direccion: ${r2d2Pos[2]}")
+    println("x: ${r2d2Pos[0]}, y: ${r2d2Pos[1]}, direccion: ${mostrarDireccion(r2d2Pos[2])}")
 
     r2d2Pos= arturito(-10,-5,2)
-    println("x: ${r2d2Pos[0]}, y: ${r2d2Pos[1]}, direccion: ${r2d2Pos[2]}")
+    println("x: ${r2d2Pos[0]}, y: ${r2d2Pos[1]}, direccion: ${mostrarDireccion(r2d2Pos[2])}")
 
-    r2d2Pos= arturito(-10,-5,-2,4,-8)
-    println("x: ${r2d2Pos[0]}, y: ${r2d2Pos[1]}, direccion: ${r2d2Pos[2]}")
+    r2d2Pos= arturito(-10,-5,2,4,-8)
+    println("x: ${r2d2Pos[0]}, y: ${r2d2Pos[1]}, direccion: ${mostrarDireccion(r2d2Pos[2])}")
 }
 fun arturito(vararg move: Int): List<Int> {
     var ejeX = 0
@@ -24,8 +24,8 @@ fun arturito(vararg move: Int): List<Int> {
     for (pasos in move) {
         when (direc){
             0-> ejeY += pasos
-            1-> ejeX += pasos
-            2-> ejeY += pasos
+            1-> ejeX -= pasos
+            2-> ejeY -= pasos
             3-> ejeX += pasos
         }
         if (direc==3) direc = 0 else direc +=1
